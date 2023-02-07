07cd543a7311833aae2854a1635563538f64c58b
Jeff Worker <>
2023-02-07 12:24:52 +0100

    socket tickets application

---
+++ models/ticket-control.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const fs = require('fs');
+
+class Ticket {
+    constructor( numero, escritorio ){
+        this.numero = numero;
+        this.escritorio = this.escritorio;
+    }
+}
+
+class TicketControl {
+
+    constructor() {
+        this.ultimo = 0;
+        this.hoy = new Date().getDate();
+        this.tickets = [];
+        this.ultimos4 = [];
+
+        this.init();
+    } 
+
+    get toJson() {
+        return{
+            ultimo: this.ultimo,
+            hoy: this.hoy,
+            tickets: this.tickets,
+            ultimos4: this.ultimos4,
+        }
+    }
+
+    init () {
+        const { hoy, tickets, ultimo, ultimos4 } = require('../db/data.json');
+        if( hoy === this.hoy ) {
+            this.tickets = tickets;
+            this.ultimo = ultimo;
+            this.ultimos4 = ultimos4;
+        } else {
+            // Es otro día
+            this.guardarDB()
+        }
+    }
+
+    guardarDB() {
+
+        const dbPath = path.join( __dirname, '../db/data.json' );
+        fs.writeFileSync( dbPath, JSON.stringify( this.toJson ) );
+    }
+
+    siguiente() {
+        this.ultimo +=1
+        const ticket = new Ticket( this.ultimo, null );
+        this.tickets.push( ticket );
+
+        this.guardarDB();
+        return 'Ticket ' + ticket.numero;
+    }
+
+    atenderTicket( escritorio ) {
+
+        // No tenemos tickets 
+        if ( this.tickets.length === 0 ) {
+            return null;
+        }
+
+        const ticket = this.tickets.shift(); //this.tickets[0]; //Borra el primer ticket y devuelve el ticket borrado.
+        ticket.escritorio = escritorio;
+
+        this.ultimos4.unshift( ticket ); //añade ticket al inicio del array
+
+        if( this.ultimos4.length > 4 ) {
+            this.ultimos4.splice(-1,1); // corta el ultimo elemento
+        }
+
+        this.guardarDB();
+
+        return ticket;
+    }
+}
+
+
+module.exports = TicketControl;
